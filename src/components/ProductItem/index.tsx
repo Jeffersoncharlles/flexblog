d3c173f9478553019aa5c79af253c6bcf066a114
jeffersoncharlles <contato@jefferdeveloper.com>
2022-01-15 22:06:20 -0300

    chore: adicionais styles

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -1,9 +1,18 @@
 import styles from './styles.module.scss'
+import produto1 from '../../assets/produtos1.jpg';
 
-export const ProductItem = () => {
+interface ProductProps {
+    color: string;
+    img: string;
+    name: string;
+}
+
+export const ProductItem = ({ color, img, name }: ProductProps) => {
 
     return (
-        <>
-        </>
+        <div className={styles.produtosItem}>
+            <h2>{name}</h2>
+            <img src={img} alt={name} />
+        </div>
     );
 }
